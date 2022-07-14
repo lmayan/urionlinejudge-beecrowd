70e013d79262281307efe787f468d4603dbea1dc
lmayan <lucasmayanaec@gmail.com>
2022-07-14 23:35:00 +0200

    beecrowd 1006

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double a = sc.nextDouble() * 2;
+		double b = sc.nextDouble() * 3;
+		double c = sc.nextDouble() * 5;
+		
+		double media = (a + b + c) / 10;
+
+		System.out.printf("MEDIA = %.1f%n", media);
+	}
+}
